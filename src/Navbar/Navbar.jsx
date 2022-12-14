7a5862135afbb3d5088f9d0fe2dcd1b63b9fa848
nhd2121 <hoangduongng42@gmail.com>
2022-12-14 10:36:55 +0700

    first commit

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,12 @@
 import topLogoNavbar from "../assets/top-logo-navbar.png";
+import {
+  Search,
+  Thumbnail_2,
+  Notification,
+  Settings,
+  UserAvatar,
+  ChevronDown,
+} from "@carbon/icons-react";
 import "./Navbar.scss";
 
 export default function Navbar() {
@@ -11,32 +19,32 @@ export default function Navbar() {
         <div class="items">
           <div class="btn-item ngan-sach-nam left-option">
             Ngân sách năm
-            <i class="icon-left-menu fa-sharp fa-solid fa-chevron-down"></i>
+            <ChevronDown className="icon-left-menu" />
           </div>
           <div class="btn-item ngan-sach-thang left-option">
             Ngân sách tháng
-            <i class="icon-left-menu fa-sharp fa-solid fa-chevron-down"></i>
+            <ChevronDown className="icon-left-menu" />
           </div>
           <div class="btn-item bao-cao left-option">
             Báo cáo
-            <i class="icon-left-menu fa-sharp fa-solid fa-chevron-down"></i>
+            <ChevronDown className="icon-left-menu" />
           </div>
           <div class="btn-item danh-muc left-option">
             Danh mục
-            <i class="icon-left-menu fa-sharp fa-solid fa-chevron-down"></i>
+            <ChevronDown className="icon-left-menu" />
           </div>
           <div class="btn-item phan-quyen left-option">
             Phân quyền
-            <i class="icon-left-menu fa-sharp fa-solid fa-chevron-down"></i>
+            <ChevronDown className="icon-left-menu" />
           </div>
         </div>
       </div>
       <div class="right-side">
-        <i class="icon-right-menu fa-sharp fa-solid fa-magnifying-glass"></i>
-        <i class="icon-right-menu fa-brands fa-buromobelexperte"></i>
-        <i class="icon-right-menu fa-regular fa-bell"></i>
-        <i class="icon-right-menu fa-solid fa-gear"></i>
-        <i class="icon-right-menu fa-regular fa-circle-user"></i>
+        <Search className="icon-right-menu" />
+        <Thumbnail_2 className="icon-right-menu" />
+        <Notification className="icon-right-menu" />
+        <Settings className="icon-right-menu" />
+        <UserAvatar className="icon-right-menu" />
       </div>
     </div>
   );
